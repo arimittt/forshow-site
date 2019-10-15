74515ed12a0d842d58bb5443def015d0403e9eb5
Arimit Bhattacharya <arimitb@gmail.com>
2019-10-15 21:45:06 +0200

    Connected to PostgreSQL.

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
+const { Client } = require('pg');
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 let port = process.env.PORT || 3000;
 
+client.connect();
 app.use(express.static('public'));
 
 app.get('/', function(req,res) {
